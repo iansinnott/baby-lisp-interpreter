a0985ccb4324c1e041d8eb041dfee37832d5d256
Ian Sinnott <ian@iansinnott.com>
2020-08-10 10:23:31 +0800

    Reinit from lein with app template, add tokenizer

--- src/baby_lisp_interpreter/core.clj
+++ src/baby_lisp_interpreter/core.clj
@@ -1,8 +1,32 @@
-(ns baby-lisp-interpreter.core)
+(ns baby-lisp-interpreter.core
+  (:gen-class)
+  (:require [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn tokenize [^String src]
+  (-> src
+      (str/replace #"(\(|\))" " $1 ")
+      (str/split #"\s+")
+      (->> (filter #(not (empty? %))))))
 
-(foo)
+(defn parse [& args] (first args))
+
+(defn traverse [& args] (first args))
+
+(defn transform [& args] (first args))
+
+(defn generate-code [& args] (first args))
+
+
+(defn -compile [^String src]
+  (-> src
+      (tokenize)
+      (parse)
+      (transform)
+      (generate-code)))
+
+(defn -main
+  "Compile and eval a baby lisp language"
+  [& args]
+  (clojure.pprint/pprint (-compile "(add 1 2)")))
+
+(-main)
